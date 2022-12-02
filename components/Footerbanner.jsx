8840507af330273cb382b9e18050da2cd35f7971
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-12-02 10:51:20 +0100

    update footerBanner 6

--- components/Footerbanner.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const FooterBanner = () => {
-  return (
-    <div>Formulaire de contact</div>
-  )
-}
-
-export default FooterBanner;
--- components/index.js
+++ components/index.js
@@ -6,5 +6,4 @@ export { default as Footer } from "./Footer";
 export { default as Layout } from "./Layout";
 export { default as Navbar } from "./Navbar";
 export { default as Cart } from "./Cart";
-export { default as FooterBanner } from "./FooterBanner";
 
