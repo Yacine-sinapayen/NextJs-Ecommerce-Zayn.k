5c9aabd90b32997266b5548bd10fbc5b424bd482
Yacine Sinapayen <yacine.sinapayen@hotmail.fr>
2022-12-02 10:49:25 +0100

    update footerBanner 5

--- components/index.js
+++ components/index.js
@@ -5,5 +5,6 @@ export { default as Contact } from "./Contact";
 export { default as Footer } from "./Footer";
 export { default as Layout } from "./Layout";
 export { default as Navbar } from "./Navbar";
-export { default as FooterBanner } from "./FooterBanner";
 export { default as Cart } from "./Cart";
+export { default as FooterBanner } from "./FooterBanner";
+
